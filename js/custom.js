31a1df579f269c2c68822b1c95fb200a95e6e811
MohamedElsayed24 <mohamedelsayedhussein0@gmail.com>
2019-09-15 13:28:44 +0200

    last version

--- js/custom.js
+++ js/custom.js
@@ -42,6 +42,24 @@ $(document).ready(function(){
     }
   });
 
+  var arLang = $header.find('.menu .lang .arabic-lang a');
+
+  arLang.on('click', function(){
+    $('link[href="vendors/bootstrap.css"]').attr('href', 'vendors/bootstrap-rtl.css');
+    $('link[href="css/responsive.css"]').attr('href', 'css/responsive-rtl.css');
+    $('link[href="css/style.css"]').attr('href', 'css/style-rtl.css');
+    
+  });
+
+  var enLang = $header.find('.menu .lang > a');
+
+  enLang.on('click', function(){
+    $('link[href="vendors/bootstrap-rtl.css"]').attr('href', 'vendors/bootstrap.css');
+    $('link[href="css/responsive-rtl.css"]').attr('href', 'css/responsive.css');
+    $('link[href="css/style-rtl.css"]').attr('href', 'css/style.css');
+    
+  });
+
   });
 
 //  init aos animation library
